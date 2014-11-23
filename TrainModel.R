0cd521a9d736ae59a793b2beb0f540b87d73551b
Jamie Hall <jamie@kaggle.com>
2014-11-23 12:43:30 -0800

    More stockfish; cv 190.3

--- TrainModel.R
+++ TrainModel.R
@@ -63,6 +63,10 @@ yTrainBig$AverageBC <- (yTrainBig$AverageElo/2500)**2
 
 featureColumnNames <- c('gameLength', 'gameDrift', 'gameOscillation',
                         'whiteGoodShare', 'blackGoodShare', 'whiteBlunders', 'blackBlunders',
+                        'SamplePoint1', 'SamplePoint2', 'SamplePoint3', 'SamplePoint4', 'SamplePoint5',
+                        'SamplePoint6', 'SamplePoint7', 'SamplePoint8', 'SamplePoint9', 'SamplePoint10',
+                        'SamplePoint11', 'SamplePoint12', 'SamplePoint13', 'SamplePoint14', 'SamplePoint15',
+                        'SamplePoint16', 'SamplePoint17', 'SamplePoint18', 'SamplePoint19', 'SamplePoint20',
                         'Move1', 'Move2', 'Move3', 'Move4', 'Move5',
                         'OutOfBook', 'Result')
 xTestBigMatrix <- model.matrix(as.formula(paste('~ 0 +',
